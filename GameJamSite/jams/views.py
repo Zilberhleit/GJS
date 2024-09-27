d9c9e3f60e64d7a9ee9b25d77e6f7cc3cf44bc94
Mark <zilberhleitmark@mail.ru>
2024-09-27 12:49:01 +0300

    added pages folder with jams_pages folder and errors folder inside

--- GameJamSite/jams/views.py
+++ GameJamSite/jams/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 def jam_list_views(request: HttpRequest) -> HttpResponse:
-    return render(request, 'jams.html')
+    return render(request, 'pages/jams_pages/jams.html')
 
 def jam_load(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Load your game")
