246e2c272fb9d2eaed03cc0622a89572dd74a550
Mark <zilberhleitmark@mail.ru>
2024-10-01 17:59:06 +0300

    in models added model gamejam (not final solution)

--- GameJamSite/jams/models.py
+++ GameJamSite/jams/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class GameJams (models.Model):
+    title = models.CharField(max_length=255)
+    date_start = models.DateTimeField("start jam")
+    date_end = models.DateTimeField("end jam")
+    theme = models.TextField()
+    jam_slug = models.SlugField(
+        max_length=255, db_index=True, unique=True, verbose_name="URL"
+    )
