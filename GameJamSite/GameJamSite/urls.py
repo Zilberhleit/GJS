2c976380ee6ef5f810a87205d10475f2ca138d34
Mark <zilberhleitmark@mail.ru>
2024-10-02 17:57:41 +0300

    updated urls, base.html

--- GameJamSite/GameJamSite/urls.py
+++ GameJamSite/GameJamSite/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from jams import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jams/', views.jam_list_views),
-    path('load/', views.jam_load)
+    path('', include('jams.urls')),
 ]
--- GameJamSite/jams/views.py
+++ GameJamSite/jams/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 
 def jam_list_views(request: HttpRequest) -> HttpResponse:
     return render(request, 'pages/jams_pages/jams.html')
-
-def jam_load(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Load your game")
