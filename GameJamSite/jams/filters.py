1b9d2936a56566dbf4d387ef9003b6e4f621c006
Ilya <iliya.odyvan4ikys@gmail.com>
2025-03-01 14:27:20 +0300

    fix date filters in jams list page

--- GameJamSite/jams/filters.py
+++ GameJamSite/jams/filters.py
@@ -5,8 +5,10 @@ from .models import GameJams
 
 class GameJamsFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Название джема')
-    date_start = django_filters.DateFilter(field_name='date_start', lookup_expr='exact')
-    date_end = django_filters.DateFilter(field_name='date_end', lookup_expr='exact')
+
+    date_start = django_filters.DateFilter(field_name='date_start', label='Дата начала джема', lookup_expr='gte')
+    date_end = django_filters.DateFilter(field_name='date_end', label='Дата окончания джема', lookup_expr='lte')
+
     status = django_filters.ChoiceFilter(field_name='status', lookup_expr='exact', choices=GameJams.jam_status)
 
     class Meta:
