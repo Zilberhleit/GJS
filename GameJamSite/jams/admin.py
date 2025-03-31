ebbf6bda70d77ae6887c7d8f0edc96c2254d703b
Ilya <iliya.odyvan4ikys@gmail.com>
2025-03-31 17:13:39 +0300

    add countdown for gamejam detail page

--- GameJamSite/jams/admin.py
+++ GameJamSite/jams/admin.py
@@ -1,6 +1,30 @@
+import random
+from datetime import datetime, timedelta
+
 from django.contrib import admin
+
 from jams.models import GameJam, Game, RatingUserJam
 
-admin.site.register(GameJam)
+
+def rand_date():
+    start = datetime.now()
+    end = start + timedelta(days=3)
+    return start + (end - start) * random.random()
+
+
+class GameJamAdmin(admin.ModelAdmin):
+    actions = ('set_actual_start_and_end_dates',)
+
+    @admin.action(description='Change Start Date')
+    def set_actual_start_and_end_dates(self, request, queryset):
+        for gamejam in queryset:
+            if gamejam.status == 'PR' or gamejam.status == 'OG':
+                gamejam.date_start = rand_date()
+                gamejam.date_end = gamejam.date_start + timedelta(days=random.randint(3, 14))
+                gamejam.save()
+        self.message_user(request, "Start date updated successfully.")
+
+
+admin.site.register(GameJam, GameJamAdmin)
 admin.site.register(Game)
 admin.site.register(RatingUserJam)
